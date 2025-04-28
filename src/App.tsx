d691bde339cf1129f91d61dc215afe1c63d69ee9
Rafael Vieira <rafaelvieira1720@gmail.com>
2025-04-27 21:05:03 -0300

    feat: add main styles and layout for the application

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,40 @@
+import styles from './App.module.scss'
+
 function App() {
-  return <h1>AAAAAAAA</h1>
+  return (
+    <div className={styles.body}>
+      <div className={styles.column}>
+        <h2>Backlog</h2>
+        {new Array(15).fill('').map((_, index) => (
+          <p key={index}>Item</p>
+        ))}
+      </div>
+      <div className={styles.column}>
+        <h2>Em andamento</h2>
+        {new Array(15).fill('').map((_, index) => (
+          <p key={index}>Item</p>
+        ))}
+      </div>
+      <div className={styles.column}>
+        <h2>PR</h2>
+        {new Array(15).fill('').map((_, index) => (
+          <p key={index}>Item</p>
+        ))}
+      </div>
+      <div className={styles.column}>
+        <h2>Teste</h2>
+        {new Array(15).fill('').map((_, index) => (
+          <p key={index}>Item</p>
+        ))}
+      </div>
+      <div className={styles.column}>
+        <h2>Em produção</h2>
+        {new Array(15).fill('').map((_, index) => (
+          <p key={index}>Item</p>
+        ))}
+      </div>
+    </div>
+  )
 }
 
 export default App
