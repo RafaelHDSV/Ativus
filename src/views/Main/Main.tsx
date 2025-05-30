c126b83986ca496ec4488974e653c329b7f0800c
Rafael Vieira <rafaelvieira1720@gmail.com>
2025-05-29 23:17:01 -0300

    feat: implement main layout with navbar and modal for adding columns

---
+++ src/views/Main/Main.tsx
@@ -0,0 +1,84 @@
+import { Button, Form, Input, Modal } from 'antd'
+import { useState } from 'react'
+import Column from '../../components/Column/Column'
+import { useColumns } from '../../components/Column/columnFunctions'
+import styles from './Main.module.scss'
+
+export default function Main() {
+  const [form] = Form.useForm()
+  const [createColumnModalVisible, setCreateColumnModalVisible] =
+    useState(false)
+
+  const {
+    columns,
+    setColumns,
+    addNewColumn,
+    resetMockedColumns,
+    cleanColumns
+  } = useColumns()
+
+  return (
+    <>
+      <Modal
+        open={createColumnModalVisible}
+        title='Adicionar Coluna'
+        okText='Adicionar'
+        onOk={() => form.submit()}
+        onCancel={() => setCreateColumnModalVisible(false)}
+        closable={false}
+        width={400}
+        centered
+      >
+        <Form
+          form={form}
+          layout='vertical'
+          onFinish={(values) => {
+            addNewColumn({
+              values,
+              setCreateColumnModalVisible
+            })
+          }}
+          className={styles.createColumnForm}
+        >
+          <Form.Item
+            name='name'
+            label='Nome da Coluna'
+            rules={[{ required: true, message: 'Insira o nome da coluna' }]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item name='description' label='Descrição da Coluna'>
+            <Input />
+          </Form.Item>
+        </Form>
+      </Modal>
+
+      <main className={styles.main}>
+        <nav className={styles.navbar}>
+          <div onClick={resetMockedColumns} className={styles.imageLogo} />
+
+          <div className={styles.buttons}>
+            <Button
+              type='primary'
+              className='button'
+              onClick={() => setCreateColumnModalVisible(true)}
+            >
+              Adicionar coluna
+            </Button>
+
+            <Button type='default' className='button' onClick={cleanColumns}>
+              Limpar colunas
+            </Button>
+          </div>
+        </nav>
+
+        <div className={styles.columnsContainer}>
+          {columns?.map((column) => (
+            <Column key={column._id} column={column} setColumns={setColumns} />
+          ))}
+        </div>
+      </main>
+    </>
+  )
+}
