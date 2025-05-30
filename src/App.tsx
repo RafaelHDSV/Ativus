837d6f9c053c2e1438dcb7064fe986711e47023d
Rafael Vieira <rafaelvieira1720@gmail.com>
2025-05-29 23:17:07 -0300

    refactor: remove column management and layout from App component, integrating Main view instead

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,11 @@
-import { useState } from 'react'
-import Column from './components/Column/Column'
-import styles from './App.module.scss'
-import { MOCKED_VALUES } from './components/Column/columnConstants'
-import { IColumn } from './components/Column/columnInterfaces'
+import { ConfigProvider } from 'antd'
+import { AntdConfigsTheme } from './constants/AntdConfigs'
+import Main from './views/Main/Main'
 
 export default function App() {
-  const [columns, setColumns] = useState<IColumn[] | []>(MOCKED_VALUES)
-
-  function handleClick() {
-    const newColumn = {
-      _id: Date.now(),
-      title: `Coluna ${columns.length + 1}`,
-      items: ['Novo item 1', 'Novo item 2']
-    }
-
-    setColumns(prevColumns => [...prevColumns, newColumn])
-  }
-
-  function resetMockedColumns() {
-    setColumns(MOCKED_VALUES)
-  }
-
   return (
-    <main className={styles.app}>
-      <nav className={styles.navbar}>
-        <div onClick={resetMockedColumns} className={styles.imageLogo}></div>
-
-        <div className={styles.buttons}>
-          <button className='primaryButton' onClick={handleClick}>
-            Adicionar coluna
-          </button>
-          <button className='secondaryButton' onClick={() => setColumns([])}>
-            Limpar colunas
-          </button>
-        </div>
-      </nav>
-
-      <div className={styles.columnsContainer}>
-        {columns?.map(column => (
-          <Column key={column._id} column={column} setColumns={setColumns} />
-        ))}
-      </div>
-    </main>
+    <ConfigProvider theme={AntdConfigsTheme}>
+      <Main />
+    </ConfigProvider>
   )
 }
