945673215b87b47a178926058d735622cb0042ab
Rafael Vieira <rafaelvieira1720@gmail.com>
2025-05-30 01:01:36 -0300

    feat: enhance addNewColumn function with message API integration for user feedback

--- src/components/Column/columnFunctions.ts
+++ src/components/Column/columnFunctions.ts
@@ -1,3 +1,4 @@
+import dayjs from 'dayjs'
 import { useState } from 'react'
 import { MOCKED_VALUES } from './columnConstants'
 import { IAddNewColumnProps, IColumn } from './columnInterfaces'
@@ -6,20 +7,31 @@ export function useColumns() {
   const [columns, setColumns] = useState<IColumn[]>(MOCKED_VALUES)
 
   function addNewColumn({
+    form,
     values,
-    setCreateColumnModalVisible
+    setCreateColumnModalVisible,
+    messageApi
   }: IAddNewColumnProps) {
-    setCreateColumnModalVisible(false)
+    try {
+      const newColumn: IColumn = {
+        _id: Math.floor(Math.random() * 1000),
+        title: values.name,
+        description: values.description,
+        items: [],
+        lastUpdated: dayjs().format('DD/MM/YYYY HH:mm')
+      }
 
-    const newColumn: IColumn = {
-      _id: Math.floor(Math.random() * 1000),
-      title: values.name,
-      description: values.description,
-      items: [],
-      lastUpdated: new Date().toLocaleString()
+      setColumns((prevColumns) => [...prevColumns, newColumn])
+      messageApi.success({
+        content: 'Coluna adicionada com sucesso!',
+        duration: 2
+      })
+    } catch (err) {
+      console.error(err)
+    } finally {
+      form.resetFields()
+      setCreateColumnModalVisible(false)
     }
-
-    setColumns((prevColumns) => [...prevColumns, newColumn])
   }
 
   function resetMockedColumns() {
