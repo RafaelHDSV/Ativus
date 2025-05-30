769e01a1cd4027dbfca3a29fd650f221c14e0a74
Rafael Vieira <rafaelvieira1720@gmail.com>
2025-05-29 23:17:48 -0300

    refactor: update IColumn interface to correct property types and enhance structure

--- src/components/Column/columnInterfaces.ts
+++ src/components/Column/columnInterfaces.ts
@@ -1,6 +1,17 @@
 export interface IColumn {
   _id: number
   title: string
-  lastUpdated?: string
+  description?: string
   items: string[]
+  lastUpdated: string
+}
+
+export interface IColumnFormValues {
+  name: string
+  description?: string
+}
+
+export interface IAddNewColumnProps {
+  values: IColumnFormValues
+  setCreateColumnModalVisible: (visible: boolean) => void
 }
