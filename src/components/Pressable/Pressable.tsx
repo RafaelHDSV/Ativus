06725dc5515daf765d98693dae779a8a3be7b056
Rafael Vieira <rafaelvieira1720@gmail.com>
2025-05-30 01:37:50 -0300

    feat: add Pressable component with customizable icon and hover animation

---
+++ src/components/Pressable/Pressable.tsx
@@ -0,0 +1,34 @@
+import clsx from 'clsx'
+import React, { forwardRef } from 'react'
+import styles from './Pressable.module.scss'
+
+export type IPressableProps = Omit<
+  React.DetailedHTMLProps<
+    React.ButtonHTMLAttributes<HTMLButtonElement>,
+    HTMLButtonElement
+  >,
+  'ref'
+> & {
+  icon?: boolean
+  animateHover?: boolean
+}
+
+export const Pressable = forwardRef<HTMLButtonElement, IPressableProps>(
+  ({ className = '', children, icon, animateHover = true, ...props }, ref) => {
+    return (
+      <button
+        type='button'
+        ref={ref}
+        className={clsx(
+          styles.pressable,
+          icon && styles['pressable--icon'],
+          animateHover && styles['pressable--animated-hover'],
+          className
+        )}
+        {...props}
+      >
+        {children}
+      </button>
+    )
+  }
+)
