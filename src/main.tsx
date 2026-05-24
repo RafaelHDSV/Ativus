29631aa33087101decefd32e23670a8c8b8a88f6
Rafael Vieira <rafaelvieira1720@gmail.com>
2026-05-23 22:16:06 -0300

    feat: add vieira analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { VieiraAnalytics } from '@vieira/analytics/react'
 import App from './App.tsx'
 import './global.scss'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <VieiraAnalytics projectKey="ativus" />
   </StrictMode>
 )
